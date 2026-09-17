40454ae50dc4960dc5c05c1b9e0bd53a626d2e36
Talket <dobariyatalket@gmail.com>
2026-09-17 15:34:01 +0530

    Fix Vercel serverless invocation by exporting Express app directly

--- Web/api/index.ts
+++ Web/api/index.ts
@@ -1,8 +1,6 @@
 import app from "../server/app";
 
-// Export Vercel Serverless Function handler
-// Accepts standard Node (req, res) or Express app invocation
-export default function handler(req: any, res: any) {
-  return app(req, res);
-}
+// Export the Express application directly as the default export for Vercel Serverless
+export default app;
+
 
